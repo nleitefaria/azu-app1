8ad25bcf0525e65233adc5424cb98bdaeffe8d66
User01 <User01@BPW3XT2>
2020-11-24 13:30:16 +0000

    Added POST to Product

--- src/main/java/com/nleitefaria/controller/ProductController.java
+++ src/main/java/com/nleitefaria/controller/ProductController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nleitefaria.domain.ProductDTO;
+import com.nleitefaria.model.Product;
 import com.nleitefaria.service.ProductService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,5 +28,19 @@ public class ProductController {
 	{
 		return new ResponseEntity<List<ProductDTO>>(productService.findAll(), HttpStatus.OK);	
 	}
-
+	
+	@Operation(summary = "Create a product")
+	@PostMapping("/api/v1/products")
+	public ResponseEntity<Product> save(@RequestBody ProductDTO productDTO) 
+	{
+	    Product product = productService.save(productDTO);		
+		if(product != null)
+		{
+			return new ResponseEntity<Product>(productService.save(productDTO), HttpStatus.CREATED);		
+		}
+		else
+		{
+			return new ResponseEntity<Product>(productService.save(productDTO), HttpStatus.BAD_REQUEST);
+		}	  
+	}
 }
