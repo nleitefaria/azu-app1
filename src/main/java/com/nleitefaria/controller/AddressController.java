687d23b9c117fd5a0fdab7275f45cd9f3f02ffc6
User01 <User01@BPW3XT2>
2020-11-24 16:18:12 +0000

    Added POST to Address, Company, Customer and Product

--- src/main/java/com/nleitefaria/controller/AddressController.java
+++ src/main/java/com/nleitefaria/controller/AddressController.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nleitefaria.domain.AddressDTO;
+import com.nleitefaria.domain.ProductDTO;
+import com.nleitefaria.model.Address;
+import com.nleitefaria.model.Product;
 import com.nleitefaria.service.AddressService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,6 +29,21 @@ public class AddressController
 	public ResponseEntity<List<AddressDTO>> findAll()
 	{
 		return new ResponseEntity<List<AddressDTO>>(addressService.findAll(), HttpStatus.OK);	
+	}
+	
+	@Operation(summary = "Create a address")
+	@PostMapping("/api/v1/address")
+	public ResponseEntity<Address> save(@RequestBody AddressDTO addressDTO) 
+	{
+		Address address = addressService.save(addressDTO);		
+		if(address != null)
+		{
+			return new ResponseEntity<Address>(addressService.save(addressDTO), HttpStatus.CREATED);		
+		}
+		else
+		{
+			return new ResponseEntity<Address>(addressService.save(addressDTO), HttpStatus.BAD_REQUEST);
+		}	  
 	}
 
 }
--- src/main/java/com/nleitefaria/controller/CompanyController.java
+++ src/main/java/com/nleitefaria/controller/CompanyController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nleitefaria.domain.CompanyDTO;
+import com.nleitefaria.model.Company;
 import com.nleitefaria.service.CompanyService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,5 +27,20 @@ public class CompanyController {
 	public ResponseEntity<List<CompanyDTO>> findAll()
 	{
 		return new ResponseEntity<List<CompanyDTO>>(companyService.findAll(), HttpStatus.OK);	
+	}
+	
+	@Operation(summary = "Create a company")
+	@PostMapping("/api/v1/company")
+	public ResponseEntity<Company> save(@RequestBody CompanyDTO companyDTO) 
+	{
+	    Company company = companyService.save(companyDTO);		
+		if(company != null)
+		{
+			return new ResponseEntity<Company>(companyService.save(companyDTO), HttpStatus.CREATED);		
+		}
+		else
+		{
+			return new ResponseEntity<Company>(companyService.save(companyDTO), HttpStatus.BAD_REQUEST);
+		}	  
 	}
 }
